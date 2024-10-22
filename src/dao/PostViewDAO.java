5da988b52f81a932950f8e62d30620bedb011cf6
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-22 17:22:06 +0900

    POST관련 VO, DAO 완료

--- src/dao/PostViewDAO.java
+++ src/dao/PostViewDAO.java
@@ -1,4 +1,45 @@
 package dao;
+
+import common.Common;
+import vo.PostsVO;
+
+import java.sql.*;
+import java.util.Scanner;
+
 // 명호 사용 DAO 자바 파일
 public class PostViewDAO {
+    Connection conn = null;
+    Statement stmt = null;
+    PreparedStatement psmt = null;
+    ResultSet rs = null;
+    Scanner sc = null;
+    public PostViewDAO() {
+        sc = new Scanner(System.in);
+    }
+
+    public PostsVO viewPost(int postNo) {
+        PostsVO post = new PostsVO();
+        try {
+            conn = Common.getConnection();  // 오라클 DB 연결
+            stmt = conn.createStatement();
+            String sql = "select * from POSTS where POSTNO = '" + postNo + "'";
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                post.setPostno(rs.getInt("POSTNO"));
+                post.setTitle(rs.getString("TITLE"));
+                post.setContent(rs.getString("CONTENT"));
+                post.setAuthor(rs.getString("NNAME"));
+                post.setDate(rs.getString("PDATE"));
+                post.setCategory(rs.getString("CATE"));
+            }
+        }catch (Exception e) {
+            System.out.println(e + " 의 이유로 연결 실패");
+        } finally {
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+        }
+        return post;
+    }
+
 }
--- src/vo/PostsVO.java
+++ src/vo/PostsVO.java
@@ -71,4 +71,14 @@ public class PostsVO {
     public String getDate() {
         return date;
     }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
 }
