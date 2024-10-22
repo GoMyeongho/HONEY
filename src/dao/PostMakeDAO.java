3aa488d6e458dce87bb4c673664a6d1259ab9940
Cook <cook961004@gmail.com>
2024-10-22 16:46:37 +0900

    VO file updated

--- src/dao/PostMakeDAO.java
+++ src/dao/PostMakeDAO.java
@@ -1,11 +1,17 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 //국형씨가 사용할 DAO 자바 파일
 public class PostMakeDAO {
     Connection conn = null;
     Statement stmt = null;
+    PreparedStatement psmt = null;
+    ResultSet rs = null;
+    Scanner sc = null;
 
 }
