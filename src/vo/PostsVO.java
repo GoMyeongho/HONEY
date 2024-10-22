51910d379136b49dde1cc4149a17960054c766e9
suddengithub <konyu1007@gmail.com>
2024-10-22 16:30:05 +0900

    test 16

--- src/vo/PostsVO.java
+++ src/vo/PostsVO.java
@@ -1,6 +1,5 @@
 package vo;
 
 public class PostsVO {
-    public static void main(String[] args) {
-    }
+    main
 }
--- src/vo/UsersVO.java
+++ src/vo/UsersVO.java
@@ -83,5 +83,5 @@ public class UsersVO {
         this.pwKey = pwKey;
     }
 
-    testtest
+    testtesttesttest
 }
