176e47120c7cd42c5c2423f6e2e766034761c495
Cook <cook961004@gmail.com>
2024-10-23 10:51:48 +0900

    VO file updated

--- src/controller/PostMakeController.java
+++ src/controller/PostMakeController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import java.util.Scanner;
+
 public class PostMakeController {
+
+    public void postMakeMenu() {
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+
+        while (isRunning) {
+            System.out.println("===== 글 작성 메뉴 ======");
+            System.out.println("=======================");
+            printCateSelection();   // 카테고리 메뉴 호출
+        }
+    }
+
+    public void printCateSelection() {
+        System.out.println("======카테고리 목록======");
+        System.out.println("1. ");
+        System.out.print("원하시는 글의 해당하는 카테고리를 선택 해 주세요 : ");
+    }
+
 }
--- src/dao/PostMakeDAO.java
+++ src/dao/PostMakeDAO.java
@@ -1,17 +1,42 @@
 package dao;
 
+import common.Common;
+import vo.PostsVO;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.Scanner;
+
 
 //국형씨가 사용할 DAO 자바 파일
 public class PostMakeDAO {
     Connection conn = null;
-    Statement stmt = null;
     PreparedStatement psmt = null;
-    ResultSet rs = null;
-    Scanner sc = null;
+
+    public PostMakeDAO() {
+    }
+
+    public boolean postInsert(PostsVO vo) { // 글 작성시 쿼리 포맷
+        // 글 작성 할때 시간이 자동으로 적용 되므로 쿼리 내에 SYSDATE 로 대체후 CATE 부분을 앞당겨서 입력 받음
+        String sql = "INSERT INTO POSTS(POSTNO, TITLE, NNAME, PCONTERNT, PDATE, CATE) VALUES(POSTS_SEQ.NEXTVAL,?,?,?,SYSDATE,?)";
+        try {
+            conn = Common.getConnection();
+            psmt = conn.prepareStatement(sql);
+            //psmt.setInt(1, vo.getPostno());                   // 시퀀스 쿼리 사용으로 필요없음
+            psmt.setString(1, vo.getTitle());       // 글 제목 입력값 데이터베이스 전달
+            psmt.setString(2, vo.getAuthor());      // 글 쓴이 입력값 데이터베이스 전달
+            psmt.setString(3, vo.getContent());     // 글 본문 입력값 데이터베이스 전달
+            //psmt.setDate(5, vo.getDate());                    // DATE 또한 쿼리로 받기 때문에 필요 없음
+            psmt.setString(4, vo.getCategory());    // 카테고리 입력값 데이터베이스 전달
+            psmt.executeUpdate();   // SQL 쿼리 실행
+            return true;
+        } catch (Exception e) {
+            System.out.println("글 작성에 실패 하였습니다.");
+            return false;
+        } finally { // psmt -> conn 순서로 데이터베이스 닫기
+            Common.close(psmt);
+            Common.close(conn);
+        }
+    }
 
 }
+
