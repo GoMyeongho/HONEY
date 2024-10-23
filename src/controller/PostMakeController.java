1cfe0b1d795990115b3490866acb9a0e07329032
Cook <cook961004@gmail.com>
2024-10-23 12:18:38 +0900

    VO file updated

--- src/controller/PostMakeController.java
+++ src/controller/PostMakeController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import dao.CategoryDAO;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class PostMakeController {
-
+    private CategoryDAO categoryDAO = new CategoryDAO();
+    Scanner sc = new Scanner(System.in);
     public void postMakeMenu() {
-        Scanner sc = new Scanner(System.in);
         boolean isRunning = true;
 
         while (isRunning) {
@@ -16,9 +19,19 @@ public class PostMakeController {
     }
 
     public void printCateSelection() {
+        List<String> categories = categoryDAO.getCategories();  // 카테고리 DAO 에서 만든 메서드 출력
         System.out.println("======카테고리 목록======");
-        System.out.println("1. ");
-        System.out.print("원하시는 글의 해당하는 카테고리를 선택 해 주세요 : ");
+        for (int i = 0; i < categories.size(); i++) {
+            System.out.println((i + 1) + "- " + categories.get(i)); // for 문으로 카테고리를 번호와 카테고리로 출력
+        }
+        System.out.print("카테고리를 선택 해 주세요 : ");
+        int cateOpt = sc.nextInt(); // 카테고리 입력 값을 변수에 담아둠
+        sc.nextLine();  // 겹치는 줄 제거
+        System.out.println(categories + "를 선택 하셨습니다.");
+    }
+
+    public void writeTitle() {
+        System.out.println();
     }
 
 }
