9f66a151d1fae208bfa95f9229dbfb8a56b98c91
GoMyeongho <sdx02013@ajou.ac.kr>
2024-10-23 09:50:15 +0900

    여러가지 추가

--- src/dao/CommentsDAO.java
+++ src/dao/CommentsDAO.java
@@ -1,4 +1,37 @@
 package dao;
 
+import common.Common;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class CommentsDAO {
+    Connection conn = null;
+    Statement stmt = null;
+    PreparedStatement psmt = null;
+    ResultSet rs = null;
+    Scanner sc = null;
+    public CommentsDAO() {
+        sc = new Scanner(System.in);
+    }
+
+    public HashSet<Integer> name2Comments(String nName) {
+        HashSet<Integer> set = new HashSet<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT POSTNO FROM COMMENTS WHERE NNAME = '"+ nName + "';");
+            while (rs.next()) set.add(rs.getInt("POSTNO"));
+        }catch (Exception e) {
+            System.out.println(e + "의 이유로 연결에 실패했습니다.");
+        }
+        return set;
+    }
+
+
+
 }
--- src/dao/LikedDAO.java
+++ src/dao/LikedDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
+import common.Common;
+import vo.LikesVO;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class LikedDAO {
@@ -17,8 +21,38 @@ public class LikedDAO {
         sc = new Scanner(System.in);
     }
 
+    public HashSet<LikesVO> likeSet (String nName) {
+        HashSet<LikesVO> set = new HashSet<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT POSTNO FROM LIKES WHERE NNAME = '"+ nName + "';");
+            while (rs.next()) new LikesVO(rs.getInt("POSTNO"));
+        }catch (Exception e) {
+            System.out.println(e + "의 이유로 연결에 실패했습니다.");
+        }
+        return set;
+    }
 
-    public String isLiked(String nName, int postNo) {
+    public HashSet<LikesVO> likeSet (int postNo) {
+        HashSet<LikesVO> set = new HashSet<>();
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery("SELECT NNAME FROM LIKES WHERE POSTNO = "+ postNo + ";");
+            while (rs.next()) new LikesVO(postNo, rs.getString("NNAME"));
+        }catch (Exception e) {
+            System.out.println(e + "의 이유로 연결에 실패했습니다.");
+        }
+        return set;
+    }
 
+    public String likeMark(HashSet<LikesVO> set, int POSTNO) {
+        for (LikesVO vo : set) {
+            if (vo.getPostNo() == POSTNO) {
+                return heart[1];
+            }
+        }
+        return heart[0];
     }
 }
