44f2eb95bcdaf8ff29e91dec9930cedae1a3a3aa
suddengithub <konyu1007@gmail.com>
2024-10-23 15:06:49 +0900

    Update MyPageDAO.java

--- src/dao/MyPageDAO.java
+++ src/dao/MyPageDAO.java
@@ -54,6 +54,85 @@ public class MyPageDAO {
         System.out.println("제시어 문제 : " + cui.getPwLOCK());
         System.out.println("제시어 답 : " + cui.getPwKey());
         System.out.println("=".repeat(24));
+    }
+    // 내 정보 수정
+    public void usersUpdate(UsersVO cui, String userID) {
+        // 중복확인을 위해 만들어둔 전체 회원정보를 리스트로 담아주는 selectUsersInfo 활용
+        List<UsersVO> uv1 = uDao.selectUsersInfo();
+
+        // 수정할 비밀번호 입력
+        String userPW = "";
+        while(true) {
+            System.out.print("변경할 비밀번호(8자 이상 20자 이하)(기존 비밀번호 유지는 no 입력) : ");
+            userPW = sc.next();
+
+            Pattern passPattern1 = Pattern.compile("!@#$%^&*-+{1,2}$");
+            Matcher passMatcher1 = passPattern1.matcher(userPW);
+
+            if(userPW.equalsIgnoreCase("no")) break;
+            else if (userPW.length() < 8) System.out.println("비밀번호는 8자 이상 입력해주세요.");
+            else if (userPW.getBytes().length > 20) System.out.print("비밀번호는 20자 이하 영문자와 특수문자(&를 제외)로 입력해주세요.");
+            else if (!passMatcher1.find()) System.out.println("비밀번호는 영문자, 숫자, 특수기호만 사용 할 수 있습니다.");
+            else if (userPW.indexOf('&') >= 0) System.out.println("&는 비밀번호로 사용할 수 없습니다.");
+            else break;
+        }
+
+        // 수정할 닉네임 입력
+        String nName;
+        while(true) {
+            System.out.print("변경할 닉네임을 입력하세요 (기존 닉네임 유지는 no 입력) : ");
+            nName = sc.next();
+            String check = nName;
+
+            int intN = nName.getBytes().length;
+
+            //중복 체크
+            if (uv1.stream().filter(n -> check.equals(n.getnName())).findAny().orElse(null) !=null) {
+                System.out.println("이미 사용중인 닉네임입니다.");
+            } else if (nName.equalsIgnoreCase("no")) {
+                nName = cui.getnName();
+                break;
+            }
+            else if (intN < 2) System.out.println("닉네임은 2자 이상 입력해주세요");
+            else if (intN > 30) System.out.println("닉네임은 30자 미만으로 입력");
+            else break;
+        }
+
+        // 수정할 휴대폰번호 입력
+        String phone = "";
+        while (true) {
+            System.out.println("변경할 휴대폰번호를 입력하세요 (기존 휴대폰번호 유지는 no 입력) : ");
+            phone = sc.next();
+            String check = phone;
+
+            //중복 체크
+            if (uv1.stream().filter(n -> check.equals(n.getPhone())).findAny().orElse(null) !=null){
+                System.out.println("이미 사용중인 휴대폰번호입니다.");
+            } else if (phone.equalsIgnoreCase("no")) {
+                phone = cui.getPhone();
+                break;
+            }
+            else if (phone.length() !=13) System.out.println("휴대폰번호는 (-)포함 13글자로 작성해주세요");
+            else break;
+        }
+
+        // 수정할 제시어 문제 입력
+        String pwlock = "";
+        while (true) {
+            System.out.println("변경할 제시어 문제를 입력하세요 (기존 제시어 문제 유지는 no 입력) : ");
+            pwlock = sc.next();
+            String check = pwlock;
+
+            //중복 체크
+            if (uv1.stream().filter(n -> check.equals(n.getPwLOCK())).findAny().orElse(null) !=null) {
+                System.out.println("이미 사용중인 제시어 문제입니다.");
+            } else if (pwlock.equalsIgnoreCase("no")) {
+                pwlock = cui.getPwLOCK();
+                break;
+            }
+            else if (pwlock.length() != )
+
+        }
 
 
 
@@ -61,3 +140,4 @@ public class MyPageDAO {
 
 
     }
+}
